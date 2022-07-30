d5cd058f89ea9f8dff96d865c323b4d751bb8348
swagat <swagatpadhi03@gmail.com>
2022-07-31 00:28:46 +0530

    added first c program

---
+++ helloswagat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    printf("hello swagat \n");
+    int age = 19;
+    int age2 =20;
+    printf("%d your %d age is \n",age2,age );
+    printf("my age will be %d \n ", age2 );
+    char hash = '#';
+    printf("character is %c \n",hash);
+    return 0;
+}
