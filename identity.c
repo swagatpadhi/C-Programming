ca004fc8de10ce30f98f6cd7c538c48d7813b56d
swagat <swagatpadhi03@gmail.com>
2022-07-31 20:05:55 +0530

    identity code

---
+++ identity.c
@@ -0,0 +1,16 @@
+//print hello your name
+//print your age is something
+//print your name starts with S
+// print welcome to programming
+//happy programming
+#include<stdio.h>
+int main(){
+printf("hello Swagat \n");
+int age = 19;
+printf("your age is %d \n",age);
+char letter='S';
+printf("your name starts with %c \n",letter);
+printf("welcome to programming \n");
+printf("happy programming \n");
+return 0;
+}
