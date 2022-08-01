3d88314d4b9c838a86005d12db7ef0f4cabbf5f0
swagat <swagatpadhi03@gmail.com>
2022-08-01 18:48:13 +0530

    added 2 new codes

---
+++ 3.ProgramToCheckEvenNumber.c
@@ -0,0 +1,11 @@
+//write a program to check
+// if a number is divisible by 2 or not(tough for me)
+
+#include<stdio.h>
+int main(){
+int number;
+printf("please enter your number :");
+scanf("%d", &number);
+printf("%d",number%2==0);
+return 0;
+}
