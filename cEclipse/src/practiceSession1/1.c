9b40592791fc6b6acc1e5eb4a1a474223f7d4a19
swagat <swagatpadhi03@gmail.com>
2022-08-21 15:33:28 +0530

    added display program

--- cEclipse/src/practiceSession1/1.c
+++ cEclipse/src/practiceSession1/1.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
-int main() {
+#include<string.h>
 
-	printf("enter a number:");
+//Write a C program to enter your name, date of birth. and mobile number.
+//and display them in console.
+void displayDetails(){
+	char name[100];
+	char dob[50];
+	int mobileNo;
+
+	printf("enter name :\n");
 	fflush(stdout);
-	int number, i, a = -1, b = 1;
-	scanf("%d", &number);
-	for (i = 1; i <= number; i++) {
-		int c = a + b;
-		printf("%d ", c);
-		a = b;
-		b = c;
-	}
-	return 0;
+	fgets(name,100,stdin);
+
+	printf("enter date of birth in dd/mm/yyyy format \n");
+	fflush(stdout);
+	fgets(dob,50,stdin);
+
+	printf("enter mobile number : ");
+	fflush(stdout);
+	scanf("%d", &mobileNo);
+
+	printf("Hello %s Your details are:\n",name);
+	//doubt . why there is a new line after %s?
+	printf("name :%s",name);
+	printf("dob :%s",dob);
+	printf("mobile no : %d\n", mobileNo);
 }
 
---
+++ cEclipse/src/practiceSession1/mainRunner.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+void displayDetails();
+
+int main(){
+	displayDetails();
+
+	return 0;
+}
